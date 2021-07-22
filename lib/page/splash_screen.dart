151b1fb60753792a076a0b8beb1b9b81896a7ea9
Arief Syahroni <riefsyah31@gmail.com>
2021-07-22 11:37:05 +0700

    done modified splashScreen and add some assets or support

---
+++ lib/page/splash_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:pie_cake_dark/theme.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: blackColor,
+      body: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(
+              41,
+              20,
+              41,
+              0,
+            ),
+            child: Image.asset(
+              'assets/chef.png',
+              width: 293,
+              height: 474,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Image.asset(
+                  'assets/love.png',
+                  width: 66,
+                  height: 66,
+                ),
+              ],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                padding: EdgeInsets.only(bottom: 0),
+                height: 265,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: semiBlackColor,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(20),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(top: 50),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'PieLove Anetto',
+                        style: titleTextStyle,
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        'Let\'s taste the Pie Cake made\nby Chef Bimo Semesta',
+                        style: subTitleTextstyle,
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Container(
+                        height: 55,
+                        width: MediaQuery.of(context).size.width - (2 * 42),
+                        child: RaisedButton(
+                          onPressed: () {},
+                          color: orangeColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Text(
+                            'Let’s Order',
+                            style: titleTextStyle.copyWith(
+                              fontSize: 20,
+                              color: blackColor,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color blackColor = Color(0XFF000000);
+Color semiBlackColor = Color(0XFF2D2D2D);
+Color darkGreyColor = Color(0XFF89898A);
+Color orangeColor = Color(0XFFFF9500);
+Color whiteColor = Color(0XFFFFFFFF);
+
+TextStyle titleTextStyle = GoogleFonts.poppins(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+  color: whiteColor,
+);
+
+TextStyle subTitleTextstyle = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  color: darkGreyColor,
+);
